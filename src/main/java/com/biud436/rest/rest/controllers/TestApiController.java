f5cc6d51427209ea1d2fc5953be63170f673a6d9
biud436 <biud436@gmail.com>
2022-08-21 14:33:44 +0900

    feat: 기본 세팅 및 서버 가동 테스트 완료

---
+++ src/main/java/com/biud436/rest/rest/controllers/TestApiController.java
@@ -0,0 +1,14 @@
+package com.biud436.rest.rest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestApiController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "스프링 부트 프로젝트 테스트";
+    }
+
+}
