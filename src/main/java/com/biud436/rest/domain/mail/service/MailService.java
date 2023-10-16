9619fe19c896c6d1315cad15c59e43d32ac6ba9b
biud436 <biud436@gmail.com>
2023-10-16 14:54:37 +0900

    refactor: fixed send method

--- src/main/java/com/biud436/rest/domain/mail/service/MailService.java
+++ src/main/java/com/biud436/rest/domain/mail/service/MailService.java
@@ -1,10 +1,12 @@
 package com.biud436.rest.domain.mail.service;
 
+import com.amazonaws.http.SdkHttpMetadata;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.SendEmailResult;
 import com.biud436.rest.domain.mail.dto.CreateEmailDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,7 +19,11 @@ public class MailService {
         final SendEmailResult sendEmailResult =
                 amazonSimpleEmailService.sendEmail(createEmailDto.toSendRequestDto());
 
-        if (sendEmailResult.getSdkHttpMetadata().getHttpStatusCode() == 200) {
+        final SdkHttpMetadata metadata = sendEmailResult.getSdkHttpMetadata();
+        final int statusCode = metadata.getHttpStatusCode();
+        final int HTTP_OK = HttpStatus.OK.value();
+
+        if (statusCode == HTTP_OK) {
             log.info("Email has been sent successfully.");
         } else {
             log.info("Email has been failed to send.");
